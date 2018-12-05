a9c39827735443021e86340eab3bc2dd4a4fd85a
Michael Simonson <michaels@healthfinch.com>
2018-12-04 20:12:31 -0600

    setup app

---
+++ src/advofcode/core.clj
@@ -0,0 +1,7 @@
+(ns advofcode.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/advofcode/core_test.clj
@@ -0,0 +1,7 @@
+(ns advofcode.core-test
+  (:require [clojure.test :refer :all]
+            [advofcode.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
